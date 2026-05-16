d637eb106bb91f774002389e6e6ad12f465d8a8a
Guhszvv <vruum962@gmail.com>
2026-05-16 14:41:43 -0300

    style: Translated error message

--- frontend/src/modules/localStorage.ts
+++ frontend/src/modules/localStorage.ts
@@ -1,7 +1,7 @@
 export async function updateLocalStorage(url: string) {
   const response = await fetch(url);
   if (!response) {
-    throw new Error("Erro ao pegar as últimas páginas!");
+    throw new Error("Error retrieving last pages!");
   }
   const data = await response.json();
   return data.forEach(([title, last_page]: [string, number]) => {
